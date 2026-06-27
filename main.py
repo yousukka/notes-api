c3b582ebe8b29803e6c11d316f02f5b9e6dc9b6d
yousukka <kayousuf2004@gmail.com>
2026-06-27 20:34:28 +0530

    Day 3 Create and List Notes

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+notes = []
+
+class Note(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.post("/notes")
+def create_note(note: Note):
+    notes.append(note)
+    return note
+
+@app.get("/notes")
+def list_notes():
+    return notes
